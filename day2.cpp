f3f55fb2bfe04968edd9375d0c07bfa1ba8f25a3
youssefazazy6969-sketch <youssefazazy6969@gmail.com>
2026-04-28 19:27:19 +0300

    Create day2.cpp

---
+++ day2.cpp
@@ -0,0 +1,9 @@
+// the day 2 for learning c++ programming language
+#include <iostream>
+//comment
+int main(){
+    std::cout<</*<= this is character output */"I finishe level 4 unit 3 of the english course and now i will start the program course!\n";
+    return 0;
+}
+/* i practicing c++ syntax ,comments and outputting strings */
+/* i finished tasks this day*/
